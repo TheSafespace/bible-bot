3f97cb9925d338794d19d10dd24c84608e50e83d
Sebastian Schimpfhauser <seschi98@googlemail.com>
2020-09-08 21:19:12 +0200

    Code Cleanup

--- index.js
+++ index.js
@@ -1,79 +1,84 @@
 require('dotenv').config();
 
-const Discord = require('discord.js');
-const googleTTS = require('node-google-tts-api');
-const tts = new googleTTS();
 const fs = require('fs');
 const crypto = require('crypto');
 const _ = require('lodash');
 
+const Discord = require('discord.js');
+const GoogleTTS = require('node-google-tts-api');
+
 const bibleApi = require('./bibleApi');
 
-async function randomVerse(books) {
-    const chapterIds = books.data.map(book => book.chapters.map(chapter => chapter.id)).flat();
-    const randomChapterID = _.sample(chapterIds);
+const tts = new GoogleTTS();
 
-    const verses = await bibleApi.fetchVerses(randomChapterID);
-    const verseIds = verses.data.map(verse => verse.id).flat();
-    const randomVerse = _.sample(verseIds);
+const getRandomVerse = async (books) => {
+  const chapterIds = books.data
+    .map((book) => book.chapters.map((chapter) => chapter.id))
+    .flat();
+  const randomChapterID = _.sample(chapterIds);
 
-    const myVerse = await bibleApi.fetchVerseContent(randomVerse);
-    return myVerse.data.content.trim();
-}
+  const verses = await bibleApi.fetchVerses(randomChapterID);
+  const verseIds = verses.data.map((verse) => verse.id).flat();
+  const randomVerse = _.sample(verseIds);
 
-function getSHA256(input) {
-    return crypto.createHash('sha256').update(input).digest('hex');
-}
+  const myVerse = await bibleApi.fetchVerseContent(randomVerse);
+  return myVerse.data.content.trim();
+};
+
+const getSHA256 = (input) => {
+  return crypto.createHash('sha256').update(input).digest('hex');
+};
 
 const main = async () => {
+  console.log('Starting BibleBot for Discord....');
 
-    console.log("Starting BibleBot for Discord....");
+  console.log('Creating TTS directory');
+  fs.mkdirSync('./tts', { recursive: true });
 
-    console.log("Creating TTS directory");
-    fs.mkdir("./tts", function(err) {});
+  console.log('Initializing Discord Client');
+  const client = new Discord.Client();
 
-    console.log("Initializing Discord Client");
-    const client = new Discord.Client();
+  console.log('Pre-fetching books from bible API');
+  const books = await bibleApi.fetchBooks();
 
-    console.log("Pre-fetching books from bible API");
-    const books = await bibleApi.fetchBooks();
+  client.on('ready', () => {
+    console.log(`Logged in to Discord as ${client.user.tag}!`);
+  });
 
-    client.on('ready', () => {
-      console.log(`Logged in to Discord as ${client.user.tag}!`);
-    });
+  client.on('message', async (msg) => {
+    if (msg.content === '!bible') {
+      msg.reply(await getRandomVerse(books));
+    }
 
-    client.on('message', async msg => {
-      if (msg.content === '!bible') {
-        msg.reply(await randomVerse(books));
-      }
+    if (msg.content === '!biblejoin') {
+      if (msg.member.voice.channel) {
+        const text = await getRandomVerse(books);
+        const ttsResult = await tts.get({
+          text,
+          lang: 'de',
+          limit_bypass: text >= 200,
+        });
 
-      if (msg.content === '!biblejoin') {
-        if (msg.member.voice.channel) {
-          const text = await randomVerse(books);
-          tts.get({
-		  text: text,
-		  lang: 'de',
-		  limit_bypass: (text >= 200) ? true : false
-	  }).then(arr => {
-	      const data = (arr >= 200) ? tts.concat(arr) : arr;
- 	      const path = './tts/' + getSHA256(data) + '.mp3';
-	      if (!fs.existsSync(path)) {
-	          fs.writeFileSync(path, data);
-	      }
-	      msg.member.voice.channel.join().then(connection => {
-		  const dispatcher = connection.play(path);
-		  dispatcher.on('finish', () => {
-		      dispatcher.destroy();
-		      connection.disconnect();
-		  });
-	      });
-          });
-        } else {
-          msg.reply("Du bist nicht im Channel");
+        // If the result length is less than 200 chars we get a string/buffer, otherwise an array
+        const data = ttsResult >= 200 ? tts.concat(ttsResult) : ttsResult;
+
+        const path = `./tts/${getSHA256(data)}.mp3`;
+        if (!fs.existsSync(path)) {
+          fs.writeFileSync(path, data);
         }
+        msg.member.voice.channel.join().then((connection) => {
+          const dispatcher = connection.play(path);
+          dispatcher.on('finish', () => {
+            dispatcher.destroy();
+            connection.disconnect();
+          });
+        });
+      } else {
+        msg.reply('Du bist nicht im Channel');
       }
-    });
-    client.login(process.env.DISCORD_BOT_TOKEN);
-}
+    }
+  });
+  client.login(process.env.DISCORD_BOT_TOKEN);
+};
 
 main();
